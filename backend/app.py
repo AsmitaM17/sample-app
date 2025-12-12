8efedd2ee57baa3254ad6c20e79d0235fa1c0eba
ASMITA MUKHERJEE <et22bthcs102@kazirangauniversity.in>
2025-12-12 17:56:28 +0530

    error handling

--- backend/app.py
+++ backend/app.py
@@ -2,9 +2,14 @@ from flask import Flask
 from flask_cors import CORS
 from routes.auth_routes import auth_bp
 from routes.student_routes import student_bp
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
-CORS(app)
+
+# Enable CORS for all routes and origins
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 app.register_blueprint(auth_bp)
 app.register_blueprint(student_bp)
